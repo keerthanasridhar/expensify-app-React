7bd60b11189626c44d1a6bbd45e0ae23aa61a49a
Keerthana sridhar <keerthasri93@gmail.com>
2018-12-19 11:33:55 -0800

    Changes all the dev to dependancy

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname,'..','public')
-const port = process.env.PORT || 3008;
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3009;
+
 app.use(express.static(publicPath));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log('Server is up!');
+});
 
 
-//to make sure that it doesn't throw a 404 when you refresh
-app.get('*',(req,res)=>{
-res.sendFile(path.join(publicPath,'index.html')); //path
-});
-app.listen(port,()=>{
-    console.log(`server is up!`);
-});
+
+
+
