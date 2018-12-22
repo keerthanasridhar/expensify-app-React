3a37a77781add5e89c9adfea55a6335a2c30646a
Keerthana sridhar <keerthasri93@gmail.com>
2018-12-21 22:19:13 -0800

    Some nitty gritty

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -6,7 +6,7 @@ import selectExpenses from '../selectors/expenses';
 export const ExpenseList =(props)=>(
 <div>
 {
-    props.expenses.length === 0 ? (<p>NO EXPENSES!</p>
+    props.expense.length === 0 ? (<p>NO EXPENSES!</p>
     ) :
      (
         props.expenses.map((expense)=>{
