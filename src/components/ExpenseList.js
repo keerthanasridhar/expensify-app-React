c54a94053889388f6da80cbd72e40354c82ac78d
Keerthana sridhar <keerthasri93@gmail.com>
2018-12-21 21:30:25 -0800

    Made some conditional statments in expenseList

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,13 +3,13 @@ import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
-const ExpenseList =(props)=>(
+export const ExpenseList =(props)=>(
 <div>
 {
-    props.expenses.length === 0 ? (<p>No Expenses!</p>) :
-    (
-        props.expenses.map((expense) => {
-          return <ExpenseListItem key={expense.id} {...expense} />;
+    props.expenses.length ===0 ? (<p>No EXPENSES!</p>) :
+     (
+        props.expenses.map((expense)=>{
+            return <ExpenseListItem key={expense.id} {...expense}/>
         })
     )
 }
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import {Link } from 'react-router-dom';
 
-
-
-
-
-
 const ExpenseListItem = ({id,description ,amount,createdAt })=>{
     return (
         <div>
